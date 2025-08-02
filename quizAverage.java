0745a2e653cf16a62849d354b5bd2eb55dcb8e5e
cunros <79799304+cunros@users.noreply.github.com>
2025-08-02 23:01:38 +0800

    Update quizAverage.java

--- quizAverage.java
+++ quizAverage.java
@@ -5,70 +5,75 @@ public class quizAverage
     
     public static void main(String[] args) 
     {
-        Scanner scanner = new Scanner(System.in);
-        boolean sigma = false;
+
+        boolean sigma;
         double q1=0.0;
         double q2=0.0;
         double q3=0.0;
         boolean again = false;
-        boolean confrm = false;
+        boolean confrm;
 
+        try(Scanner scanner = new Scanner(System.in))
+        {
         
-        while(!again)
-        {   
+            while(!again)
+            {   
 
-            sigma = false;
+                sigma = false;
 
-                while(!sigma)
+                    while(!sigma)
+                    {
+
+                        System.out.print("\n\nFirst quiz: ");
+                        if(scanner.hasNextDouble())            
+                        {q1 = scanner.nextDouble();sigma=true;}else{System.out.println("\nERR\nERR\n Not a numeber please input a number");scanner.next();}
+
+                    }
+
+                sigma = false;
+                
+                    while(!sigma)
+                    {
+
+                        System.out.print("Second quiz: ");
+                        if(scanner.hasNextDouble())            
+                        {q2 = scanner.nextDouble();sigma=true;}else{System.out.println("\nERR\nERR\n Not a numeber please input a number");scanner.next();}
+
+                    }
+
+                sigma = false;
+                
+                    while(!sigma)
+                    {
+
+                        System.out.print("Third quiz: ");
+                        if(scanner.hasNextDouble())            
+                        {q3 = scanner.nextDouble();sigma=true;}else{System.out.println("\nERR\nERR\n Not a numeber please input a number");scanner.next();}
+
+                    }
+
+                double sum = q1+q2+q3;
+                double ave = sum/3;    
+                System.err.println("\n\n"+ave+" is your average!");
+                
+                confrm = false;
+                while(!confrm)
                 {
-
-                    System.out.print("\n\nFirst quiz: ");
-                    if(scanner.hasNextDouble())            
-                    {q1 = scanner.nextDouble();sigma=true;}else{System.out.println("\nERR\nERR\n Not a numeber please input a number");scanner.next();}
+                    System.out.print("\n\nCalculate again?Y/N: ");
+                    if(scanner.hasNext())
+                    {
+                        String reap = scanner.next();
+                        if(reap.equalsIgnoreCase("Y")){again = false;confrm=true;}else if(reap.equalsIgnoreCase("N")){again = true;confrm=true;}else{System.err.println("Invalid input");}
+                    }
+                    else{System.err.println("ERR\nERR\n\nplease put only a number");scanner.next();}
 
                 }
 
-            sigma = false;
-            
-                while(!sigma)
-                {
 
-                    System.out.print("Second quiz: ");
-                    if(scanner.hasNextDouble())            
-                    {q2 = scanner.nextDouble();sigma=true;}else{System.out.println("\nERR\nERR\n Not a numeber please input a number");scanner.next();}
-
-                }
-
-            sigma = false;
-            
-                while(!sigma)
-                {
-
-                    System.out.print("Third quiz: ");
-                    if(scanner.hasNextDouble())            
-                    {q3 = scanner.nextDouble();sigma=true;}else{System.out.println("\nERR\nERR\n Not a numeber please input a number");scanner.next();}
-
-                }
-
-            double sum = q1+q2+q3;
-            double ave = sum/3;    
-            System.err.println("\n\n"+ave+" is your average!");
-            
-            confrm = false;
-            while(!confrm)
-            {
-                System.out.print("\n\nCalculate again?Y/N: ");
-                if(scanner.hasNext())
-                {
-                    String reap = scanner.next();
-                    if(reap.equalsIgnoreCase("Y")){again = false;confrm=true;}else if(reap.equalsIgnoreCase("N")){again = true;confrm=true;}else{System.err.println("Invalid input");}
-                }
-                else{System.err.println("ERR\nERR\n\nplease put only a number");scanner.next();}
-
-            }
-
+            }   
 
         }
+
     }
 
 }

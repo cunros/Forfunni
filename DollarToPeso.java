2cc6686e142f6b52197f6fbca4a0b44869eca08f
cunros <79799304+cunros@users.noreply.github.com>
2025-08-02 23:01:08 +0800

    Update DollarToPeso.java

--- DollarToPeso.java
+++ DollarToPeso.java
@@ -8,43 +8,45 @@ public class DollarToPeso
         boolean end = false;
         double dollarconver = 50.60;
         double dollar = 0.0;
-        Scanner scanner = new Scanner(System.in);
-
-        while (end == false)
+        try(Scanner scanner = new Scanner(System.in))
         {
-            boolean validnum = false;
-            while(validnum == false)
+
+            while (!end)
             {
-
-                System.out.print("\n\nEnter amoount of dollars: ");
-                if(scanner.hasNextDouble())
+                boolean validnum = false;
+                while(!validnum)
                 {
-                    dollar = scanner.nextDouble();
-                    validnum = true;
-                }
-                else
-                {
-                    System.out.println("ERR\nERR\n\n\nNot a number! Please input numbers only\n\n");
-                    scanner.next();
+
+                    System.out.print("\n\nEnter amoount of dollars: ");
+                    if(scanner.hasNextDouble())
+                    {
+                        dollar = scanner.nextDouble();
+                        validnum = true;
+                    }
+                    else
+                    {
+                        System.out.println("ERR\nERR\n\n\nNot a number! Please input numbers only\n\n");
+                        scanner.next();
+                    }
+
                 }
 
-            }
-
-            double pesos = dollar * dollarconver;
-            System.out.println("\n\n"+dollar+" USD is "+pesos+" in PHP\n\n");
+                double pesos = dollar * dollarconver;
+                System.out.println("\n\n"+dollar+" USD is "+pesos+" in PHP\n\n");
+                
+                System.out.print("Convert Again? Y/N: ");   
+                String repeat = scanner.next();
             
-            System.out.print("Convert Again? Y/N: ");   
-            String repeat = scanner.next();
-           
-            if(!repeat.equalsIgnoreCase("Y"))
-            {
-                end = true;
-            }
+                if(!repeat.equalsIgnoreCase("Y"))
+                {
+                    end = true;
+                }
 
 
         }
-        
-        
+
+        scanner.close();
+        }
     }
 
 }
